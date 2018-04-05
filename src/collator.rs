f1ec7087baeede8c1472b00757728b35e90e6bc3
Josiah Evans <koreanhalo@gmail.com>
2018-04-05 12:06:23 +0900

    add basic collator skeleton.

---
+++ src/collator.rs
@@ -0,0 +1,23 @@
+pub struct Collator;
+
+impl Collator {
+    fn new() -> Collator {
+        Collator
+    }
+
+    fn register();
+
+    fn check_smc();
+
+    fn get_eligibility();
+
+    fn get_collation_headers();
+
+    fn download_collations();
+
+    fn collect_proposals();
+
+    fn select_proposal();
+
+    fn add_header();
+}
