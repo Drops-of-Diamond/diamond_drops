dca369f742d062c990ef17d362a81fe9af641930
Josiah Evans <koreanhalo@gmail.com>
2018-04-05 12:02:15 +0900

    Add skeleton proposer

---
+++ src/proposer.rs
@@ -0,0 +1,19 @@
+pub struct Proposer;
+
+impl Proposer {
+    fn new() -> Proposer {
+        Proposer
+    }
+
+    fn register();
+
+    fn add_balance();
+
+    fn collect_blobs();
+
+    fn prepare_collation();
+
+    fn reveal_proposal();
+
+    fn broadcast_collation_body();
+}
