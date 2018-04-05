a8cc0e471ca31b00b8c6f557c745dc0ff6d3682d
Josiah Evans <koreanhalo@gmail.com>
2018-04-05 11:53:05 +0900

    updated lib.rs and tests for new file structure

--- src/lib.rs
+++ src/lib.rs
@@ -20,62 +20,62 @@ mod tests {
     fn it_sets_client_mode_to_proposer() {
         // Verbose command
         let test_args_verbose = vec![String::from("-mode"), String::from("proposer")];
-        let config_verbose = parse_args(test_args_verbose).unwrap();
+        let config_verbose = cli::parse_args(test_args_verbose).unwrap();
         // Short command
         let test_args_short = vec![String::from("-mode"), String::from("p")];
-        let config_short = parse_args(test_args_short).unwrap();
+        let config_short = cli::parse_args(test_args_short).unwrap();
 
-        assert_eq!(config_verbose.mode, Mode::Proposer);
-        assert_eq!(config_short.mode, Mode::Proposer);
+        assert_eq!(config_verbose.mode, config::Mode::Proposer);
+        assert_eq!(config_short.mode, config::Mode::Proposer);
     }
 
     #[test]
     fn it_sets_client_mode_to_collator() {
         // Verbose command
         let test_args_verbose = vec![String::from("-mode"), String::from("collator")];
-        let config_verbose = parse_args(test_args_verbose).unwrap();
+        let config_verbose = cli::parse_args(test_args_verbose).unwrap();
         // Short command
         let test_args_short = vec![String::from("-mode"), String::from("c")];
-        let config_short = parse_args(test_args_short).unwrap();
+        let config_short = cli::parse_args(test_args_short).unwrap();
 
-        assert_eq!(config_verbose.mode, Mode::Collator);
-        assert_eq!(config_short.mode, Mode::Collator);
+        assert_eq!(config_verbose.mode, config::Mode::Collator);
+        assert_eq!(config_short.mode, config::Mode::Collator);
     }
 
     #[test]
     fn it_sets_client_mode_to_both() {
         // Verbose command
         let test_args_verbose = vec![String::from("-mode"), String::from("both")];
-        let config_verbose = parse_args(test_args_verbose).unwrap();
+        let config_verbose = cli::parse_args(test_args_verbose).unwrap();
         // Short command
         let test_args_short = vec![String::from("-mode"), String::from("b")];
-        let config_short = parse_args(test_args_short).unwrap();
+        let config_short = cli::parse_args(test_args_short).unwrap();
         // Default mode
         let test_args_default = vec![];
-        let config_default = parse_args(test_args_default).unwrap();
+        let config_default = cli::parse_args(test_args_default).unwrap();
 
-        assert_eq!(config_verbose.mode, Mode::Both);
-        assert_eq!(config_short.mode, Mode::Both);
-        assert_eq!(config_default.mode, Mode::Both);
+        assert_eq!(config_verbose.mode, config::Mode::Both);
+        assert_eq!(config_short.mode, config::Mode::Both);
+        assert_eq!(config_default.mode, config::Mode::Both);
     }
 
     #[test]
     fn it_reports_invalid_arguments() {
         // Invalid configuration
         let test_args_configuration = vec![String::from("-bin"), String::from("collator")];
-        let error_configuration = parse_args(test_args_configuration);
+        let error_configuration = cli::parse_args(test_args_configuration);
 
         // Invalid value
         let test_args_value = vec![String::from("-mode"), String::from("bin")];
-        let error_value = parse_args(test_args_value);
+        let error_value = cli::parse_args(test_args_value);
 
         // No configuration
         let test_args_no_arg = vec![String::from("mode"), String::from("both")];
-        let error_no_arg = parse_args(test_args_no_arg);
+        let error_no_arg = cli::parse_args(test_args_no_arg);
 
         // No value
         let test_args_no_value = vec![String::from("-mode")];
-        let error_no_value = parse_args(test_args_no_value);
+        let error_no_value = cli::parse_args(test_args_no_value);
 
         assert_eq!(error_configuration, Err("Invalid configuration argument"));
         assert_eq!(error_value, Err("Invalid configuration value"));
