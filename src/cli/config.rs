2dfc08758185307cbfdcd7593a520736c53a2c0c
Luke Schoen <ltfschoen@gmail.com>
2018-04-09 12:22:20 +1000

    test: Move thread name test into implementation file as unit test

--- src/cli/config.rs
+++ src/cli/config.rs
@@ -25,3 +25,17 @@ impl Config {
         Config { mode }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_sets_thread_name_for_config_arguments() {
+        let test_proposer_name = Mode::Proposer.value();
+        let test_collator_name = Mode::Collator.value();
+
+        assert_eq!(test_proposer_name, "proposer".to_string());
+        assert_eq!(test_collator_name, "collator".to_string());
+    }
+}
