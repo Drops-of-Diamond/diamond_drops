818e41ca6a25298a74128b41ea8394bb7352bc8a
jamesray1 <16969914+jamesray1@users.noreply.github.com>
2018-04-18 18:16:04 +1000

    Update the license header in build.rs and diagrams/ml.dot.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+/* Copyright 2018 James Ray (@jamesray1), Josiah (@ChosunOne), Luke Schoen (@ltfschoen)
+
 This is free and unencumbered software released into the public domain.
 
 Anyone is free to copy, modify, publish, use, compile, sell, or
@@ -16,13 +18,13 @@ software under copyright law.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE James Ray BE LIABLE FOR ANY CLAIM, DAMAGES OR
+IN NO EVENT SHALL THE AUTHORS, James Ray BE LIABLE FOR ANY CLAIM, DAMAGES OR
 OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-
+ 
 For more information, please refer to <http://unlicense.org>
-//! ![uml](ml.svg)
+*///! ![uml](ml.svg)
 
 // External crates
 extern crate ethereum_types;
