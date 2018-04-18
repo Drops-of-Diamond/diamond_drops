0852f68e8bdf9e0d19113b480d7eaab0005e47ff
James Ray <16969914+jamesray1@users.noreply.github.com>
2018-04-18 23:34:12 +1000

    use ethereum_types;

--- src/smc_listener.rs
+++ src/smc_listener.rs
@@ -28,7 +28,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org>
 */
 
-use ethereum_types::Address;
+use ethereum_types;
 use std::sync::mpsc;
 use message;
 
