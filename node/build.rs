96b1b16b7ed8032396caaac290cbedf940d28005
James Ray <16969914+jamesray1@users.noreply.github.com>
2018-04-30 20:39:08 +1000

    Add changes from experimenting with make UML tasks.

--- node/build.rs
+++ node/build.rs
@@ -7,6 +7,6 @@ fn main() {
     let _ = mml::src2both("src", dest.replace("-", "_").as_str());
 
     // Generate diagram in /target/doc/diamond_drops_node/ml.svg
-    let dest: String = concat!("../target/doc/", env!("CARGO_PKG_NAME")).to_string();
+    let dest2: String = concat!("../target/doc/", env!("CARGO_PKG_NAME")).to_string();
     let _ = mml::src2both("src", dest.replace("-", "_").as_str());
 }
