675b06b4f1740981ee2385228e4dbbcd911fb408
jamesray1 <16969914+jamesray1@users.noreply.github.com>
2018-05-07 14:14:14 +1000

    Save work on chunks: from_bytes before checking out to complete work on error_chain

--- node/src/modules/collation/chunk.rs
+++ node/src/modules/collation/chunk.rs
@@ -1,11 +1,15 @@
+use bitreader::BitReader;
+use modules::constants::{CHUNK_SIZE, CHUNK_DATA_SIZE, 
+    COLLATION_SIZE, CHUNKS_PER_COLLATION, MAX_BLOB_SIZE};
+
 #[derive(PartialEq, Debug, Clone)]
 pub struct Chunk {
     pub indicator: u8,
-    pub data: [u8; 31]
+    pub data: [u8; CHUNK_DATA_SIZE]
 }
 
 impl Chunk {
-    pub fn new(indicator: u8, data: [u8; 31]) -> Chunk {
+    pub fn new(indicator: u8, data: [u8; CHUNK_DATA_SIZE]) -> Chunk {
         Chunk {
             indicator,
             data
@@ -14,6 +18,8 @@ impl Chunk {
 
     /// Build the indicator byte with the supplied data.  Length can be
     /// any value if the chunk is not terminal (the value is ignored)
+    }
+    /*
     pub fn build_indicator(skip_evm: bool, terminal: bool, length: u8) -> u8{
         let mut indicator: u8 = 0;
 
@@ -22,28 +28,33 @@ impl Chunk {
             indicator += 0b1000_0000;
         }
         if terminal {
-            assert!(0 < length && length < 32);
+            assert!(0 < length && length < CHUNK_SIZE);
             indicator += length;
         }
         indicator
     }
+    */
 
     /// Convert the Chunk into bytes
-    pub fn as_bytes(self) -> [u8; 32] {
-        let mut bytes: [u8; 32] = [0; 32];
+    pub fn as_bytes(self) -> [u8; CHUNK_SIZE] {
+        let mut bytes: [u8; CHUNK_SIZE] = [0; CHUNK_SIZE];
         bytes[0] = self.indicator;
-        for i in 1..32 {
+        for i in 1..CHUNK_SIZE {
             bytes[i] = self.data[i-1];
         }
         bytes
     }
 
-    /// Convert 32 bytes into a chunk
-    pub fn from_bytes(chunk_bytes: [u8; 32]) -> Chunk {
+    /// Convert CHUNK_SIZE bytes into a chunk
+    pub fn from_bytes(chunk_bytes: [u8; CHUNK_SIZE]) -> Chunk {
         let indicator = chunk_bytes[0];
-        let mut data: [u8; 31] = [0; 31];
-        for i in 1..32 {
-            data[i-1] = chunk_bytes[i];
+        let mut data: [u8; CHUNK_DATA_SIZE] = [0; CHUNK_DATA_SIZE];
+        let indicator_ref = &[chunk_bytes[0]];
+        let mut indicator_reader = BitReader::new(indicator_ref);
+
+        indicator = indicator_reader.reader_u8(3)?
+        for i in 0..CHUNK_DATA_SIZE {
+            data[i] = chunk_bytes[i+1];
         }
         Chunk {
             indicator,
@@ -58,7 +69,7 @@ mod tests {
 
     #[test]
     fn it_builds_indicator() {
-        let full_ind: u8 = Chunk::build_indicator(true, true, 31);
+        let full_ind: u8 = Chunk::build_indicator(true, true, CHUNK_DATA_SIZE);
         let correct_full_ind: u8 = 0b1001_1111;
         assert_eq!(full_ind, correct_full_ind);
         let full_non_terminal_ind: u8 = Chunk::build_indicator(true, false, 1);
@@ -74,9 +85,9 @@ mod tests {
 
     #[test]
     fn it_converts_to_bytes() {
-        let chunk = Chunk::new(0b1000_0000, [1; 31]);
+        let chunk = Chunk::new(0b1000_0000, [1; CHUNK_DATA_SIZE]);
         let chunk_bytes = chunk.as_bytes();
-        let correct_chunk_bytes: [u8; 32] = [0b1000_0000, 1, 1, 1, 
+        let correct_chunk_bytes: [u8; CHUNK_SIZE] = [0b1000_0000, 1, 1, 1, 
                                             1, 1, 1, 1, 1, 
                                             1, 1, 1, 1, 1,
                                             1, 1, 1, 1, 1, 1,
@@ -88,7 +99,7 @@ mod tests {
 
     #[test]
     fn it_converts_from_bytes() {
-        let chunk = Chunk::new(0b1000_0000, [1; 31]);
+        let chunk = Chunk::new(0b1000_0000, [1; CHUNK_DATA_SIZE]);
         let chunk_bytes = chunk.clone().as_bytes();
         let same_chunk = Chunk::from_bytes(chunk_bytes);
         assert_eq!(chunk, same_chunk);
